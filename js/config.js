0c98fdac93429c50a9c483801531106c5a131d6f
Giriprasad-s <prasad.giri.s.giri@gmail.com>
2024-11-17 23:44:33 -0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_gIAOAysM7', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '64tng6i9bpbh0h71fmo9lljaq3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
